1944693e98dbb43b49a111fea687266369af23f2
ukai <>
2001-11-25 17:41:57 +0000

    [w3m-dev 02547] "message/*" as a kind of text type From: Kiyokazu SUTO <suto@ks-and-ks.ne.jp>

--- file.c
+++ file.c
@@ -208,7 +208,8 @@ static int
 is_text_type(char *type)
 {
     return (type == NULL || type[0] == '\0' ||
-	    strncasecmp(type, "text/", 5) == 0);
+	    strncasecmp(type, "text/", 5) == 0 ||
+	    strncasecmp(type, "message/", sizeof("message/") - 1) == 0);
 }
 
 static int
