82708f71eea5bcf2ed313b347a91a19794969d01
ukai <>
2001-12-10 15:27:59 +0000

    [w3m-dev 02648] rewrite strCmp() with strcmp() Hironori Sakamoto <hsaka@mth.biglobe.ne.jp>

--- indep.c
+++ indep.c
@@ -33,14 +33,7 @@ allocStr(const char *s, int len)
 int
 strCmp(const void *s1, const void *s2)
 {
-    unsigned char *p1 = *(unsigned char **)s1;
-    unsigned char *p2 = *(unsigned char **)s2;
-
-    while ((*p1 != '\0') && (*p1 == *p2)) {
-	p1++;
-	p2++;
-    }
-    return (*p1 - *p2);
+    return strcmp(*(const char **)s1,  *(const char **)s2);
 }
 
 char *
