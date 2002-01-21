cec3cea25647011bee71ca39bdf3defd480eb7e6
ukai <>
2002-01-21 18:34:00 +0000

    [w3m-dev 02880] URI_PATTERN * url.c (URI_PATTERN): better match for % hex hex * url.c (chkExternalURIBuffer): don't use URI_PATTERN as format string From: Fumitoshi UKAI  <ukai@debian.or.jp>

--- url.c
+++ url.c
@@ -2081,7 +2081,7 @@ searchURIMethods(ParsedURL *pu)
  * escaped       = "%" hex hex
  */
 
-#define URI_PATTERN	"[-;/?:@&=+$,a-zA-Z0-9_.!~*'()%]*"
+#define URI_PATTERN	"([-;/?:@&=+$,a-zA-Z0-9_.!~*'()]|%[0-9A-Fa-f][0-9A-Fa-f])*"
 void
 chkExternalURIBuffer(Buffer *buf)
 {
@@ -2090,11 +2090,11 @@ chkExternalURIBuffer(Buffer *buf)
 
     for (i = 0; (ump = urimethods[i]) != NULL; i++) {
 	for (; ump->item1 != NULL; ump++) {
-	    reAnchor(buf, Sprintf("%s:" URI_PATTERN, ump->item1)->ptr);
+	    reAnchor(buf, Sprintf("%s:%s", ump->item1, URI_PATTERN)->ptr);
 	}
     }
     for (ump = default_urimethods; ump->item1 != NULL; ump++) {
-	reAnchor(buf, Sprintf("%s:" URI_PATTERN, ump->item1)->ptr);
+	reAnchor(buf, Sprintf("%s:%s", ump->item1, URI_PATTERN)->ptr);
     }
 }
 #endif
