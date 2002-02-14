86efa0a8dee2f5dd89cd48352ad213d22d969c55
ukai <>
2002-02-14 03:47:48 +0000

    [w3m-dev 03048] garbage of images on the right edge of termical. * display.c (redrawLineImage): fix width calculation From: Hironori Sakamoto <hsaka@mth.biglobe.ne.jp>

--- display.c
+++ display.c
@@ -677,8 +677,8 @@ redrawLineImage(Buffer *buf, Line *l, int i)
 		    h = image->height - sy;
 		else
 		    h = (int)(pixel_per_line - sy);
-		if (w > (int)((buf->COLS - rcol + column) * pixel_per_char))
-		    w = (int)((buf->COLS - rcol + column) * pixel_per_char);
+		if (w > (int)((buf->rootX + buf->COLS) * pixel_per_char - x))
+		    w = (int)((buf->rootX + buf->COLS) * pixel_per_char - x);
 		if (h > (int)(LASTLINE * pixel_per_line - y))
 		    h = (int)(LASTLINE * pixel_per_line - y);
 		addImage(cache, x, y, sx, sy, w, h);
