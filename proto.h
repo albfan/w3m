ca8d2623f736ebbdddb92f44bbbeb67fe5684376
ukai <>
2002-03-14 15:41:54 +0000

    * proto.h (readHeader): added From: Fumitoshi UKAI  <ukai@debian.or.jp>

--- proto.h
+++ proto.h
@@ -205,6 +205,7 @@ extern int checkSaveFile(InputStream stream, char *path);
 extern int checkOverWrite(char *path);
 extern char *inputAnswer(char *prompt);
 extern int matchattr(char *p, char *attr, int len, Str *value);
+extern void readHeader(URLFile *uf, Buffer *newBuf, int thru, ParsedURL *pu);
 extern char *checkHeader(Buffer *buf, char *field);
 extern Buffer *newBuffer(int width);
 extern Buffer *nullBuffer(void);
