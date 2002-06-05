1d48ce7e79e4ab574e00eb4cadb18009674633a7
ukai <>
2002-06-05 15:27:47 +0000

    [w3m-dev 03203] dict * main.c (execdict): fix loadGeneralFile() From: Hironori SAKAMOTO <hsaka@mth.biglobe.ne.jp>

--- main.c
+++ main.c
@@ -4832,10 +4832,7 @@ execdict(char *word)
 	return;
     }
     dictcmd = Sprintf("%s?%s", DictCommand, w)->ptr;
-    buf = loadGeneralFile(url_quote_conv(dictcmd, Currentbuf->document_code),
-			  baseURL(Currentbuf),
-			  parsedURL2Str(&Currentbuf->currentURL)->ptr,
-			  0, NULL);
+    buf = loadGeneralFile(dictcmd, NULL, NO_REFERER, 0, NULL);
     if (buf == NULL) {
 	disp_message("Execution failed", FALSE);
     }
