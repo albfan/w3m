b8b36620fc6ef9108066f9dc1313ae59fa2165cc
ukai <>
2002-10-01 15:34:20 +0000

    [w3m-dev 03332] Re: hang up when seeing web page that contains xbm file * w3mimg/x11/x11_w3mimg.c (x11_load_image): transparent support From: Yuji Abe <cbo46560@pop12.odn.ne.jp>

--- w3mimg/x11/x11_w3mimg.c
+++ w3mimg/x11/x11_w3mimg.c
@@ -194,13 +194,17 @@ x11_load_image(w3mimg_op * self, W3MImage * img, char *fname, int w, int h)
     if (w != iw || h != ih) {
 	GdkPixbuf *newpixbuf;
 	newpixbuf = gdk_pixbuf_scale_simple(pixbuf, w, h, GDK_INTERP_BILINEAR);
-	gdk_pixbuf_xlib_render_to_drawable(newpixbuf, (Drawable) img->pixmap,
-					   xi->imageGC, 0, 0, 0, 0, w, h,
+	gdk_pixbuf_xlib_render_to_drawable_alpha(newpixbuf,
+					   (Drawable) img->pixmap,
+					   0, 0, 0, 0, w, h,
+					   GDK_PIXBUF_ALPHA_BILEVEL, 1,
 					   XLIB_RGB_DITHER_NORMAL, 0, 0);
 	gdk_pixbuf_finalize(newpixbuf);
     } else {
-	gdk_pixbuf_xlib_render_to_drawable(pixbuf, (Drawable) img->pixmap,
-					   xi->imageGC, 0, 0, 0, 0, w, h,
+	gdk_pixbuf_xlib_render_to_drawable_alpha(pixbuf,
+					   (Drawable) img->pixmap,
+					   0, 0, 0, 0, w, h,
+					   GDK_PIXBUF_ALPHA_BILEVEL, 1,
 					   XLIB_RGB_DITHER_NORMAL, 0, 0);
     }
     gdk_pixbuf_unref(pixbuf);
