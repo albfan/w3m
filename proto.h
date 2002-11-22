59b8cded882c70e801f7340386f5917dcc733804
ukai <>
2002-11-22 15:43:14 +0000

    [w3m-dev 03458] Re: mouse menu * display.c (displayBuffer): nTab2 -> nTab, mouse_menu * fm.h (nTab2): deleted 	(MouseMenuMap): added 	(MouseMenu): added 	(LIMIT_MOUSE_MENU): added * func.c (initMouseMenu): delete mouse_menu_map initialization 		conv_from_system 		mouse_menu->width * main.c (main): mouse_menu->in_action = FALSE 	(posTab): check mouse_menu 	(mouse_menu_action): add y arg 		mouse_menu_width check 	(process_mouse): nTab2 -> nTab, mouse_menu 	(nTabLine): deleted 	(calcTabPos): check mouse_menu * menu.c (mainMn): x, y 	(selMn): mouse_menu check 	(tabMn): mosue_menu check * proto.h (nTabLine): deleted From: Hironori SAKAMOTO <hsaka@mth.biglobe.ne.jp>

--- proto.h
+++ proto.h
@@ -225,7 +225,6 @@ extern char *inputAnswer(char *prompt);
 extern int matchattr(char *p, char *attr, int len, Str *value);
 extern void readHeader(URLFile *uf, Buffer *newBuf, int thru, ParsedURL *pu);
 extern char *checkHeader(Buffer *buf, char *field);
-extern int nTabLine(void);
 extern TabBuffer *newTab(void);
 extern TabBuffer *deleteTab(TabBuffer *tab);
 extern void addDownloadList(pid_t pid, char *url, char *save, char *lock,
