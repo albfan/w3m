cb3923be117af3fd90c5d2aa13565c1b8e54a5eb
ukai <>
2003-07-22 17:27:19 +0000

    [w3m-dev 03941] fix character encoding for 'v' * main.c (vwSrc): use document_code as DisplayCode From: Hironori SAKAMOTO <hsaka@mth.biglobe.ne.jp>

--- main.c
+++ main.c
@@ -4531,12 +4531,22 @@ vwSrc(void)
     if (Currentbuf->sourcefile == NULL) {
 	if (Currentbuf->pagerSource &&
 	    !strcasecmp(Currentbuf->type, "text/plain")) {
+#ifdef JP_CHARSET
+	    char old_code;
+#endif
 	    FILE *f;
 	    Str tmpf = tmpfname(TMPF_SRC, NULL);
 	    f = fopen(tmpf->ptr, "w");
 	    if (f == NULL)
 		return;
+#ifdef JP_CHARSET
+	    old_code = DisplayCode;
+	    DisplayCode = Currentbuf->document_code;
+#endif
 	    saveBufferBody(Currentbuf, f, TRUE);
+#ifdef JP_CHARSET
+	    DisplayCode = old_code;
+#endif
 	    fclose(f);
 	    Currentbuf->sourcefile = tmpf->ptr;
 	}
