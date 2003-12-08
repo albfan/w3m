bd8cac0d7e9fd8b79f07270a263744c1308bf5f3
ukai <>
2003-12-08 16:08:01 +0000

    [w3m-dev 04021] Deeply nested table * file.c (completeHTMLstream): check obuf->table_level From: Kiyokazu SUTO <suto@ks-and-ks.ne.jp>

--- file.c
+++ file.c
@@ -6387,6 +6387,9 @@ completeHTMLstream(struct html_feed_environ *h_env, struct readbuffer *obuf)
 	HTMLlineproc1("</title>", h_env);
 
     /* for unbalanced table tag */
+    if (obuf->table_level >= MAX_TABLE)
+	obuf->table_level = MAX_TABLE - 1;
+
     while (obuf->table_level >= 0) {
 	table_mode[obuf->table_level].pre_mode
 	    &= ~(TBLM_SCRIPT | TBLM_STYLE | TBLM_PLAIN);
