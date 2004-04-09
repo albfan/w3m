84c90d416ad14b7496c798d71caba3a85a0da2c8
ukai <>
2004-04-09 17:18:49 +0000

    [w3m-dev 04063] about Makefile * Makefile.in (DEFS): use CGIBIN_DIR instead of LIB_DIR 	(MAKE_ARGS): ditto 	(distclean): rm -f * config.h.dist: use CGIBIN_DIR instead of LIB_DIR * indep.c (w3m_lib_dir): ditto * scripts/Makefile.in: ditto * scripts/multipart/Makefile.in: ditto * w3mimg/Makefile.in (clearn): remove w3mimg.a From: SAKAI Kiyotaka <ksakai@kso.netwk.ntt-at.co.jp>

--- indep.c
+++ indep.c
@@ -723,7 +723,8 @@ w3m_auxbin_dir()
 char *
 w3m_lib_dir()
 {
-    return w3m_dir("W3M_LIB_DIR", LIB_DIR);
+    /* FIXME: use W3M_CGIBIN_DIR? */
+    return w3m_dir("W3M_LIB_DIR", CGIBIN_DIR);
 }
 
 char *
