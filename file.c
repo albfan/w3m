474c3d625b6c9d6afaecdf96622444f25b47024b
inu <>
2006-06-10 10:01:18 +0000

    Show progress status when dump_extra option is specified. [w3m-dev 03992]

--- file.c
+++ file.c
@@ -6761,6 +6761,8 @@ loadHTMLstream(URLFile *f, Buffer *newBuf, FILE * src, int internal)
 	if (src)
 	    Strfputs(lineBuf2, src);
 	linelen += lineBuf2->length;
+	if (w3m_dump & DUMP_EXTRA)
+	    printf("W3m-in-progress: %s\n", convert_size2(linelen, current_content_length, TRUE));
 	if (w3m_dump & DUMP_SOURCE)
 	    continue;
 	showProgress(&linelen, &trbyte);
