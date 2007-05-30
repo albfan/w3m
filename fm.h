95c6aad959ab9f3455ba66b0ebcfa71f181a46b4
inu <>
2007-05-30 04:47:24 +0000

    fix for tab browsing. [w3m-dev 04266]

--- fm.h
+++ fm.h
@@ -523,6 +523,13 @@ typedef struct _DownloadList {
 } DownloadList;
 #define DOWNLOAD_LIST_TITLE "Download List Panel"
 
+#define COPY_BUFROOT(dstbuf, srcbuf) {\
+ (dstbuf)->rootX = (srcbuf)->rootX; \
+ (dstbuf)->rootY = (srcbuf)->rootY; \
+ (dstbuf)->COLS = (srcbuf)->COLS; \
+ (dstbuf)->LINES = (srcbuf)->LINES; \
+}
+
 #define COPY_BUFPOSITION(dstbuf, srcbuf) {\
  (dstbuf)->topLine = (srcbuf)->topLine; \
  (dstbuf)->currentLine = (srcbuf)->currentLine; \
