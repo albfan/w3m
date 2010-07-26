b8f99fbad9d7fc6e5c2a09b0b57dbb7dcc196140
htrb <>
2010-07-26 11:38:53 +0000

    * [w3m-dev 04237] [patch] cookie avoid [wrong number of dots]

--- fm.h
+++ fm.h
@@ -1112,8 +1112,10 @@ global int accept_cookie init(FALSE);
 global int accept_bad_cookie init(ACCEPT_BAD_COOKIE_DISCARD);
 global char *cookie_reject_domains init(NULL);
 global char *cookie_accept_domains init(NULL);
+global char *cookie_avoid_wrong_number_of_dots init(NULL);
 global TextList *Cookie_reject_domains;
 global TextList *Cookie_accept_domains;
+global TextList *Cookie_avoid_wrong_number_of_dots_domains;
 #endif				/* USE_COOKIE */
 
 #ifdef USE_IMAGE
