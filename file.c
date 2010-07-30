9ef2ac90105d0d7ccd221b7242bd67a8401400a3
htrb <>
2010-07-30 08:50:39 +0000

    * [w3m-dev 04345] Re: "important" bugs from bugs.debian.org * file.c (HR_ATTR_WIDTH_MAX): added. * file.c (process_hr): check w > HR_ATTR_WIDTH_MAX

--- file.c
+++ file.c
@@ -4008,13 +4008,18 @@ process_hr(struct parsed_tag *tag, int width, int indent_width)
     Str tmp = Strnew_charp("<nobr>");
     int w = 0;
     int x = ALIGN_CENTER;
+#define HR_ATTR_WIDTH_MAX 65535
 
     if (width > indent_width)
 	width -= indent_width;
-    if (parsedtag_get_value(tag, ATTR_WIDTH, &w))
+    if (parsedtag_get_value(tag, ATTR_WIDTH, &w)) {
+	if (w > HR_ATTR_WIDTH_MAX) {
+	    w = HR_ATTR_WIDTH_MAX;
+	}
 	w = REAL_WIDTH(w, width);
-    else
+    } else {
 	w = width;
+    }
 
     parsedtag_get_value(tag, ATTR_ALIGN, &x);
     switch (x) {
