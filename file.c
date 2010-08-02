7346394bf26b4d2b9965dc84b353cea05aaab8ae
htrb <>
2010-08-02 10:33:35 +0000

    Revert the change of using ATTR_TITLE if ATTR_ALT is empty.

--- file.c
+++ file.c
@@ -3228,15 +3228,10 @@ process_img(struct parsed_tag *tag, int width)
     p = remove_space(p);
     q = NULL;
     parsedtag_get_value(tag, ATTR_ALT, &q);
+    if (!pseudoInlines && (q == NULL || (*q == '\0' && ignore_null_img_alt)))
+	return tmp;
     t = q;
     parsedtag_get_value(tag, ATTR_TITLE, &t);
-    if (q == NULL || (*q == '\0' && ignore_null_img_alt)) {
-	if (!pseudoInlines && (t == NULL ||
-			       (*t == '\0' && ignore_null_img_alt)))
-	    return tmp;
-	q = t;
-    }
-  
     w = -1;
     if (parsedtag_get_value(tag, ATTR_WIDTH, &w)) {
 	if (w < 0) {
